1bec5d23b3a566c8008f5a1c675b679d30a0f8a1
Ethan Hazel <ethanyvonhazel@gmail.com>
2026-02-21 12:53:53 -0500

    Name rebrand changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import configManager from "./src/back/config-manager.js";
 import startScheduler from "./src/back/scheduler.js";
 import startAPI from "./src/back/api.js";
 
-console.log("You Up? - Created by Ethan Hazel");
+console.log("Yu - Created by Ethan Hazel");
 console.log("Version " + process.env.npm_package_version + "\n");
 (async () => {
   console.log("> Loading config...");
--- src/front/src/routes/home.jsx
+++ src/front/src/routes/home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function Home() {
   return (
     <main>
-      <h1>You up?</h1>
+      <h1>Yu</h1>
       <Link to="/guide">Guide</Link>
       <Link to="/settings">Settings</Link>
     </main>
